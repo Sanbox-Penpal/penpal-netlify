7364157de29b84bbba838b2f83bf47acd3ef87e8
Mudaafi <SleepyUndergrad@gmail.com>
2021-05-21 18:52:54 +0800

    Edits webpack for deployment

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,8 +1,7 @@
-const Dotenv = require('dotenv-webpack');
-const nodeExternals = require('webpack-node-externals');
+const Dotenv = require('dotenv-webpack')
+const nodeExternals = require('webpack-node-externals')
 
 // @see https://github.com/netlify/netlify-lambda#webpack-configuration
 module.exports = {
-  plugins: [new Dotenv()],
   externals: [nodeExternals()],
-};
+}
