67c5d4e20e6a00094cbee50b3987a58da6b83fa9
Mudaafi <SleepyUndergrad@gmail.com>
2021-05-21 19:09:08 +0800

    removes cached lambda folder

---
+++ localwebpack.functions.js
@@ -0,0 +1,8 @@
+const Dotenv = require('dotenv-webpack')
+const nodeExternals = require('webpack-node-externals')
+
+// @see https://github.com/netlify/netlify-lambda#webpack-configuration
+module.exports = {
+  plugins: [new Dotenv()],
+  externals: [nodeExternals()],
+}
