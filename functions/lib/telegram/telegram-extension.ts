b65817d6c2b4a04102da1d8cd7985cecbf74dffa
Mudaafi <SleepyUndergrad@gmail.com>
2021-05-25 16:31:36 +0800

    Adds a console.log for debugging

--- functions/lib/telegram/telegram-extension.ts
+++ functions/lib/telegram/telegram-extension.ts
@@ -85,6 +85,7 @@ export async function processTeleCallback(callback: TeleCallbackQuery) {
       metadata.stage,
       metadata.data,
     )
+    console.log(metadata, user.state, user.state.protocol)
   }
 
   const msg = callback.message
